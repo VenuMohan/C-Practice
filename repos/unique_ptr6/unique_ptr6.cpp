7882fc3a1420290ad7e691c3c9bf2a7a9bbbefe3
VenuMohan <venumohan77@gmail.com>
2026-03-14 18:10:28 +0530

    created weapon vault

--- repos/unique_ptr6/unique_ptr6.cpp
+++ repos/unique_ptr6/unique_ptr6.cpp
@@ -2,16 +2,53 @@
 //
 
 #include <iostream>
+#include "memory"
+#include "vector"
 
 class Weapon
 {
 private:
     int damage;
+    char* name;
 public:
-    Weapon(int dmg)
+    Weapon(int dmg, char* nom)
     {
         damage = dmg;
+        name = new char[strlen(nom) + 1];
+        strcpy(name, nom);
     }
+
+    Weapon(const Weapon& weapon)
+    {
+        damage = weapon.damage;
+        name = new char[strlen(weapon.name) + 1];
+        strcpy(name, weapon.name);
+    }
+
+    void PrintDamage() const
+    {
+        std::cout << damage << "\n";
+    }
+
+    const char GetWeaponName() const 
+    {
+        return *name;
+    }
+
+    ~Weapon()
+    {
+        std::cout << "Weapon Destroyed" << "\n";
+        delete[] name;
+    }
+};
+
+class Player 
+{
+private:
+    Weapon* ActiveWeapon = nullptr;
+    std::vector<std::unique_ptr<Weapon>> WeaponArray;
+
+
 };
 
 int main()
