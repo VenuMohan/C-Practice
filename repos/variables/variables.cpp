86a723ae94cf89cd96738b3bf085a3b014c56137
VenuMohan <venumohan77@gmail.com>
2026-03-16 01:56:30 +0530

    Revert "added short to variables"

--- repos/variables/variables.cpp
+++ repos/variables/variables.cpp
@@ -11,21 +11,18 @@ int main()
     std::string s;
     float f = 3.14162817;
     double d = 3.14162817;
-    short sh = 16;
     uint16_t j;
     uint32_t k;
     long l;
     bool b = -2;
 
-
     std::cout <<"int size " << sizeof(i) << std::endl;
     std::cout <<"char size " << sizeof(a) << std::endl;
     std::cout <<"string size " << sizeof(s) << std::endl;
     std::cout <<"double size " << sizeof(d) << std::endl;
-    std::cout << "short size is " << sizeof(sh) << std::endl;
     std::cout << std::setprecision(15) << "double is " << d << std::endl;
     std::cout << "float size "<<sizeof(f) << std::endl;
-    std::cout << std::setprecision(15) << "float is " << f << std::endl;
+    std::cout << std::setprecision(15) << "float is" << f << std::endl;
     std::cout << "uint16 size " << sizeof(j) << std::endl;
     std::cout << "uint32 size " <<sizeof(k) << std::endl;
     std::cout <<"long size " << sizeof(l) << std::endl;
